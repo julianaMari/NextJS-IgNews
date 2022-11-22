88a9569d225a1b2cbaee602297aefe47ce40ca7c
Juliana Cruz <julianamariacruz.jm@gmail.com>
2022-11-22 12:37:11 -0300

    Using faunaDB

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,13 +1,49 @@
+import { query as q } from 'faunadb';
+
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 
-export const authOptions = {
+import { fauna } from '../../../services/fauna';
+
+export default NextAuth({
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
     }),
   ],
-}
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const { email } = user
 
-export default NextAuth(authOptions);
+     try{
+      await fauna.query(
+        q.If(
+          q.Not(
+            q.Exists(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
+          ),
+          
+        q.Create(
+          q.Collection('users'),
+          { data:  { email } }
+        ),
+        q.Get(
+          q.Match(
+            q.Index('user_by_email'),
+            q.Casefold(user.email)
+          )
+        )
+        )
+      )
+      return true
+    } catch {
+      return false
+    }
+    },
+  }
+})
