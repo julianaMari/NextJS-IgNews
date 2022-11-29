821e77674eb22c09f8364570640de5814ab4c20b
Juliana Cruz <julianamariacruz.jm@gmail.com>
2022-11-29 00:06:40 -0300

    corrigindo bug 500

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -3,27 +3,30 @@ import { getSession } from "next-auth/react";
 import { stripe } from "../../services/stripe";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
+   
+    
     if(req.method === 'POST') {
     const session = await getSession({ req })
+    
         
-        const stripeCustomer = await stripe.customer.create({
+    const stripeCustomer = await stripe.customers.create({
             email: session.user.email,
             // metadata
         })
 
-        const stripecheckoutSession = await stripe.checkout.session.create({
-            custmer:stripeCustomer.id,
+        const stripeCheckoutSession = await stripe.checkout.sessions.create({
+            customer:stripeCustomer.id,
             payment_method_types: ['card'],
             billing_address_collection: 'required',
             line_items:[
                 { price: 'price_1M2tRwEdu5U2PHevBf1BKvjz', quantity: 1}
             ],
-            mode:'subcription',
-            allow_promotion_code: true,
+            mode:'subscription',
+            allow_promotion_codes: true,
             success_url: process.env.STRIPE_SUCESSION_URL,
             cancel_url: process.env.STRIPE_CANCEL_URL
         })
-        return res.status(200).json({ sessionId: stripecheckoutSession.id })
+        return res.status(200).json({ sessionId: stripeCheckoutSession.id })
     } else {
         res.setHeader('Allow', 'POST')
         res.status(405).end('Method not allowed')
